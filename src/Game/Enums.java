f363578ef972b68c726c238c1aeb589b3a1d9054
jfzieg <jzieg@uvm.edu>
2018-03-07 15:20:06 -0500

    Updates to Enum, GamePiece and Card

--- src/Game/Enums.java
+++ src/Game/Enums.java
@@ -1,11 +1,44 @@
 package Game;
 
+/**
+ * Enums for use by other classes
+ */
 public class Enums {
     public enum CardType{
-        ONE, TWO, THREE, FOUR, FIVE, SEVEN, EIGHT, NINE, TEN, ELEVEN, TWELVE, SORRY
+        ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SEVEN(7), EIGHT(8), NINE(9), TEN(10), ELEVEN(11), TWELVE(12), SORRY(0);
+
+        int number;
+
+        CardType(int i){
+            number = i;
+        }
+
+        /**
+         * Get the int value of the enum.
+         * @return the int value of the enum.
+         */
+        int getNumber(){
+            return number;
+        }
     }
 
+    /**
+     *
+     */
     public enum Color{
-        RED, YELLOW, GREEN, BLUE
+        RED(0), YELLOW(1), GREEN(2), BLUE(3);
+
+        int side;
+        Color(int i){
+            side = i;
+        }
+
+        /**
+         * Get the int value of the enum.
+         * @return the int value of the enum.
+         */
+        int getSide(){
+            return side;
+        }
     }
 }
