bf6de1a887c7aad26317ebe6896268924b3c8760
jfzieg <jzieg@uvm.edu>
2018-03-07 13:06:15 -0500

    Updated new class files

---
+++ src/Game/Enums.java
@@ -0,0 +1,11 @@
+package Game;
+
+public class Enums {
+    public enum CardType{
+        ONE, TWO, THREE, FOUR, FIVE, SEVEN, EIGHT, NINE, TEN, ELEVEN, TWELVE, SORRY
+    }
+
+    public enum Color{
+        RED, YELLOW, GREEN, BLUE
+    }
+}
---
+++ src/Game/Settings.java
@@ -0,0 +1,9 @@
+package Game;
+
+public class Settings {
+    public static double X_SIZE = 1280;
+    public static double Y_SIZE = 800;
+    public static double TILE_SIZE = 50;
+    public static double CARD_WIDTH = 50; // Ratio should be 5:7
+    public static double CARD_HEIGHT = 70;
+}
