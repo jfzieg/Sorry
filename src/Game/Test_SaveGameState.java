89e91481ef47a0dc45963a0565a225c966884710
Duc Anh Phan <phanducanh96@gmail.com>
2018-04-25 21:11:21 -0500

    Updates save and load functions, friendlier to user's path

--- src/Game/Test_SaveGameState.java
+++ src/Game/Test_SaveGameState.java
@@ -9,33 +9,26 @@ import javafx.embed.swing.JFXPanel;
 
 public class Test_SaveGameState {
     static void TestSave(GameState gt, Controller con) throws IOException{
-        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-        
-        File file = new File("C:\\Users\\phand\\Desktop\\CS205\\Final Project\\Sorry\\" + timeStamp +".txt");
-        
-        String[] arr = gt.loadOptions();
-        
-        gt.saveOptions(timeStamp, arr.length);
-                    
-        gt.saveGameDataToFile(file, con);
+
+        gt.saveGameDataToFile(con);
     }
-    
+
     static Controller TestLoadGameBoard(GameState gt) throws ClassNotFoundException, IOException{
-        File file = new File("C:\\Users\\phand\\Desktop\\CS205\\Final Project\\Sorry\\save.txt");
+        File file = new File("save.txt");
         Controller con2 = gt.loadControllerFromFile(file);
         return con2;
     }
-   
+
     public static void main (String[] args) throws ClassNotFoundException, IOException {
          Controller con = new Controller();
          GameState gt = new GameState();
-         
+
          TestSave(gt, con);
          Controller gt2;
-         gt2 = TestLoadGameBoard(gt);
-         
+//         gt2 = TestLoadGameBoard(gt);
+
 
          System.out.println();
-        
+
     }
 }
